0eefe513324d5e2ab255e320b919a35d9901f117
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-04-04 10:45:38 +0530

    O

--- operators/main.py
+++ operators/main.py
@@ -15,3 +15,25 @@ length = len(a)-1
 last_letter = a[length:]
 result = first_letter != last_letter
 print(result)
+##-------------------------------
+
+##check the given password length is greater than 7
+a = input()
+length = len(a)
+#int_len = int(length)
+print(length > 7)
+##------------------------------
+
+##take input A,B and check B is there in A
+a = input()
+b = input()
+length_a = len(a)
+length_b = len(b)
+last_string = a[length_a - length_b:]
+result = b == last_string
+print(result)
+
+##check profit or loss
+selling_price = int(input())
+buying_price = int(input())
+print(selling_price > buying_price)
