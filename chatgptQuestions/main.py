be32f1555bb90d7eca1a0400cd804086cada3179
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-05-08 23:45:22 +0530

    v1

--- chatgptQuestions/main.py
+++ chatgptQuestions/main.py
@@ -123,4 +123,21 @@ result = factorial(num)
 print(result)
 
 
+# Example 1:
+
+# Input: words = ["leet","code"], x = "e"
+# Output: [0,1]
+# Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+# Example 2:
+
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+# Output: [0,2]
+# Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+# Example 3:
+
+# Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+# Output: []
+# Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+
+a 012
 
