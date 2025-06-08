9e49c3616c780d6254e0df9f6518218382b9c9f0
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-06-08 22:10:28 +0530

    arr

--- practice.py
+++ practice.py
@@ -3,17 +3,27 @@
 # for i in num:
 #   print(i)
 
-## i/p ("i love india") o/p ['i', 'love', 'india']
+##i/p ("i love india") o/p ['i', 'love', 'india']
 
-text = "i love india"
-word = ""
-words = []
-for i in text:
-  if i != " ":
-    word += i
-  else:
-    words.append(word)
-    word= ""
-if(words):
-  words.append(word)
-print(words)
+# text = "i love india"
+# word = ""
+# words = []
+# for i in text:
+#   if i != " ":
+#     word += i
+#   else:
+#     words.append(word)
+#     word= ""
+# if(words):
+#   words.append(word)
+# print(words)
+
+##  i/p[1,2,3,4,5,6] o/p[0,1,2,'Hi',"Bye", "Dom", 6]
+arr = [1, 2, 3, 4, 5, 6]
+arr.insert(0, 0)            # Insert 0 at the beginning
+arr[3] = "Hi"               # Replace 3 with "Hi"
+arr[4] = "Bye"              # Replace 4 with "Bye"
+arr[5] = "Dom"              # Replace 5 with "Dom"
+
+print(arr)
+
