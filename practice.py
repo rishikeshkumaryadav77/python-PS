282cdb703b4902999065c64822d76eb37ac6d6c9
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-06-08 22:01:44 +0530

    str->arr

--- practice.py
+++ practice.py
@@ -1,4 +1,19 @@
-num = [int(i) for i in input("enter num: ").split()]
-print(num)
-for i in num:
-  print(i)
+# num = [int(i) for i in input("enter num: ").split()]
+# print(num)
+# for i in num:
+#   print(i)
+
+## i/p ("i love india") o/p ['i', 'love', 'india']
+
+text = "i love india"
+word = ""
+words = []
+for i in text:
+  if i != " ":
+    word += i
+  else:
+    words.append(word)
+    word= ""
+if(words):
+  words.append(word)
+print(words)
