5f762e8ff6c8582031adfb3a73e78164ab506b5b
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-06-20 17:38:25 +0530

    fibbNum

---
+++ fibbonacciNum/main.py
@@ -0,0 +1,24 @@
+num=10
+a,b=0,1
+arr_fibb = []
+for i in range(1, num+1):
+  arr_fibb.append(a)
+  c = a+b
+  a=b
+  b=c
+  
+print(arr_fibb)
+
+
+
+# num = 5
+# a,b=0,1
+# arr_fibb = []
+# for i in range(1, num+1):
+#   print(a)
+#   c = a+b
+  
+#   a = b
+#   b = c
+
+  
