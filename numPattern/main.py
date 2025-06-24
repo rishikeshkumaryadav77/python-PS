58d47dff5936c796f8afb99bffb7a5ed97206fd2
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-06-24 15:44:10 +0530

    reverse order num

--- numPattern/main.py
+++ numPattern/main.py
@@ -171,13 +171,13 @@
 #         1
 
 #----------------floyd Triangle
-n = 5
-k = 1
-for i in range(n):
-  for j in range(i+1):
-    print(k, end=' ')
-    k += 1
-  print()
+# n = 5
+# k = 1
+# for i in range(n):
+#   for j in range(i+1):
+#     print(k, end=' ')
+#     k += 1
+#   print()
 
 #--------output
 # 1 
@@ -186,3 +186,46 @@ for i in range(n):
 # 7 8 9 10
 # 11 12 13 14 15
 
+
+
+# --------------------
+# n = 5
+# for i in range(1, n+1):
+#   for j in range(1, i+1):
+#     print(j,end=" ")
+#   print()
+  
+#---------------- output
+# 1   
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# --------------------------
+# n = 5
+# for i in range(1, n+1):
+#   for j in range(1, i+1):
+#     print(i+j-1,end=" ")
+#   print()
+
+  # =-------o/p
+# 1 
+# 2 3
+# 3 4 5
+# 4 5 6 7
+# 5 6 7 8 9
+
+# ------------------------------------
+n = 5
+for i in range(1, n+1):
+  for j in range(0, i):
+    print(i-j,end=" ")
+  print()
+  #-----------------o/p
+# 1 
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+  
