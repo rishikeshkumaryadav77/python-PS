04b8441ef2bc328344a9a1235aa984278a7d0216
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-09-14 11:56:10 +0530

    log10countDigit

--- countDigit.py
+++ countDigit.py
@@ -1,7 +1,10 @@
+from math import *
 num = 23458765676
-count_n = 0
-while num > 0:
-  last_digit = num % 10
-  num = num // 10
-  count_n += 1
-print(count_n)
+# count_n = 0
+# while num > 0:
+#   last_digit = num % 10
+#   num = num // 10
+#   count_n += 1
+# print(count_n)
+
+print(int(log10(num)+1))
