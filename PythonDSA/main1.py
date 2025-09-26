f16e06351e3ba45a8e0bca9943b85311fec83226
rishikeshkumaryadav77 <krishikes605@gmail.com>
2025-09-26 12:54:50 +0530

    ipaddress

--- PythonDSA/main1.py
+++ PythonDSA/main1.py
@@ -1,2 +1,13 @@
-for i in range(1, 11):
-    print(f"5 * {i} = {5*i}")
+# for i in range(1, 11):
+#     print(f"5 * {i} = {5*i}")
+
+# Input: address = "1.1.1.1"
+# Output: "1[.]1[.]1[.]1"
+address = "1.1.1.1"
+replace_dot = ""
+for i in address:
+    if i == ".":
+        replace_dot += "[.]"
+    else:
+        replace_dot += i
+print(replace_dot)
