2f2d5504b46eac04220cb0bd204e4704ff12d079
rishikeshkumaryadav77 <krishikes605@gmail.com>
2026-04-05 22:32:30 +0530

    targetNum

---
+++ PythonDSA/target/main.py
@@ -0,0 +1,11 @@
+nums = [3,2,4]
+target = 6
+
+result = []
+
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            result = [i, j]
+
+print(result)
