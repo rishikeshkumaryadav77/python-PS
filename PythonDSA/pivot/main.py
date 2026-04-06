f7d9f4474455c5513e0769130cec0ac03e37a2d2
rishikeshkumaryadav77 <krishikes605@gmail.com>
2026-04-06 20:04:37 +0530

    pivot

---
+++ PythonDSA/pivot/main.py
@@ -0,0 +1,33 @@
+# Example 1:
+
+# Input: nums = [1,7,3,6,5,6]
+# Output: 3
+# Explanation:
+# The pivot index is 3.
+# Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+# Right sum = nums[4] + nums[5] = 5 + 6 = 11
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: -1
+# Explanation:
+# There is no index that satisfies the conditions in the problem statement.
+# Example 3:
+
+# Input: nums = [2,1,-1]
+# Output: 0
+# Explanation:
+# The pivot index is 0.
+# Left sum = 0 (no elements to the left of index 0)
+# Right sum = nums[1] + nums[2] = 1 + -1 = 0
+
+nums = [1,7,3,6,5,6]
+n= len(nums)
+left_sum = 0
+total = sum(nums)
+for i in range(n):
+    right_sum = total - left_sum - nums[i]
+    if left_sum == right_sum:
+        print(i) 
+    left_sum += nums[i]
+print(-1)
