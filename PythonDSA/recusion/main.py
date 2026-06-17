eab41c5faa5335c6727e3b5c52f88b3767e097ef
rishikeshkumaryadav77 <krishikes605@gmail.com>
2026-06-17 11:31:20 +0530

    recursionfibbo

--- PythonDSA/recusion/main.py
+++ PythonDSA/recusion/main.py
@@ -1,10 +1,25 @@
+def fibbo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibbo(n - 1) + fibbo(n - 2)
 
-def count(num):
-  if num == 4:
-    return
+num = int(input("Enter a number: "))
+
+if num <= 0:
+    print("Please enter a positive number")
+else:
+    for i in range(num):
+        print(fibbo(i))
+
+
+# witout recursion
+
+n = 5
+a,b = 0,1
+for i in range(n):
+  print(a)
+  c = a+b
+  a = b
+  b = c
   
-  print("rishi")
-  num += 1
-  count(num)
-
-count(0)
